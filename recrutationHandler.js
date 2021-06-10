7fe6e11b19347947684c021b66cc9bf45628b942
Dewidos <daw.po@wp.pl>
2021-06-10 17:14:58 +0200

    Another stagr

--- recrutationHandler.js
+++ recrutationHandler.js
@@ -7,7 +7,6 @@ module.exports = (message, client) => {
     
     const messageFetched = message;
 
-    console.log(messageFetched);
     let lines = messageFetched.content.split("\n");
 
     lines.forEach(l => lines[lines.indexOf(l)] = l.split(": "));
@@ -15,41 +14,43 @@ module.exports = (message, client) => {
     var name, age, why, interests, gameExperience;
 
     try {
-        if (lines.length != 3) throw new Error();
+        if (lines.length != 5) throw new Error("Za mało pól!");
 
         let nameLine = lines.find(l => ["jak masz na imię", "jak masz na imie"].includes(l[0].toLowerCase()));
 
-        if (typeof nameLine === 'undefined') throw new Error();
+        if (typeof nameLine === 'undefined') throw new Error("Nie podałeś swojego imienia!");
 
         name = nameLine[1];
 
         let ageLine = lines.find(l => l[0].toLowerCase() == "ile masz lat");
 
-        if (typeof ageLine === 'undefined') throw new Error();
+        if (typeof ageLine === 'undefined') throw new Error("Nie podałeś swojego wieku!");
 
         age = parseInt(ageLine[1]);
 
-        if (typeof age === 'undefined' || isNaN(age)) throw new Error();
+        if (typeof age === 'undefined' || isNaN(age)) throw new Error("Wiek musi być liczbą!");
+
+        if (age >= 130) throw new Error("Na pewno nie jesteś aż tak stary!");
 
         let whyLine = lines.find(l => ["dlaczego mielibyśmy ciebie wybrać?", "dlaczego mielibysmy ciebie wybrac?", "dlaczego mielibyśmy ciebie wybrać", "dlaczego mielibysmy ciebie wybrac"].includes(l[0].toLowerCase()));
 
-        if (typeof whyLine === 'undefined') throw new Error();
+        if (typeof whyLine === 'undefined') throw new Error("Musisz nam wskazać powody, aby Ciebie wybrać!");
 
         why = whyLine[1];
         
         let interestsLine = lines.find(l => ["jakie są twoje zainteresowania", "jakie sa twoje zainteresowania"].includes(l[0].toLowerCase()));
 
-        if (typeof interestsLine === 'undefined') throw new Error();
+        if (typeof interestsLine === 'undefined') throw new Error("Wypisz nam swoje zainteresowania!");
 
         interests = interestsLine[1];
 
-        let gameExperienceLine = lines.find(l => ["jakie masz doświadczenie w grach", "jakie masz doswiadczenie w grach"].includes(l[0].toLowerCase()));
+        let gameExperienceLine = lines.find(l => ["jakie masz doświadczenie w grach", "jakie masz doswiadczenie w grach", "jakie masz doświadczenie w grach?", "jakie masz doswiadczenie w grach?"].includes(l[0].toLowerCase()));
 
-        if (typeof gameExperienceLine === 'undefined') throw new Error();
+        if (typeof gameExperienceLine === 'undefined') throw new Error("Chcemy także wiedzieć jakie masz doświadczenie w grach!");
 
         gameExperience = gameExperienceLine[1];
     } catch (error) {
-        message.channel.send("Błędny format wniosku rekrutacyjnego!");
+        message.channel.send(error.message);
         return;
     }
 
