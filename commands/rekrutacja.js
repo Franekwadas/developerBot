7c8fb8986c76c5aacd2b034ea9bb37f1f7a9c3ac
FranekWadas <franiowadas@gmail.com>
2021-06-10 16:27:57 +0200

    permissions

--- commands/rekrutacja.js
+++ commands/rekrutacja.js
@@ -6,6 +6,27 @@ module.exports = {
 
     async execute(message, args, client) {
 
+        var config = client.configFile.find(c => c.guildId == message.guild.id);
+
+        if (config.moderatorRoles.length <= 0) {
+            message.channel.send("Błąd konfiguracji! Nikt nie skonfigurował roli moderatorskich.");
+            return;
+        }
+
+        
+
+        var hasPermission = false;
+
+        config.moderatorRoles.forEach(role => {
+            if (message.member.roles.cache.has(role)) {
+                hasPermission = true;
+            }
+        });
+
+        if (!hasPermission) {
+            message.channel.send("Nie masz wystarczających uprawnień!");
+            return;
+        }
         
         if (typeof args[0] !== 'undefined') {
 
